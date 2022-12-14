727fb88105f6513ed4668d04dab624899256c4fa
Milo Rue <milorue@gmail.com>
2022-12-14 09:40:31 -0800

    feat: buttongroup story cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 // Use this area as a playground/stub for testing new development
 
+import { useState } from 'react';
 import { Avatar, AvatarSize } from './components/Avatar';
+import Button from './components/Button';
 import { ButtonGroup } from './components/ButtonGroup';
 
 function App() {
+  const [value, setValue] = useState(0);
+
   return (
     <>
-      <Avatar size={AvatarSize.Large} handle={'mrue'} />
+      <Avatar circle size={AvatarSize.Large} handle={'mrue'} />
+      <ButtonGroup border value={value} onChange={(val) => setValue(val || 0)} variant={'plain'}>
+        <ButtonGroup.Option value={0}>Button 1</ButtonGroup.Option>
+        <ButtonGroup.Option value={10}>Button 2</ButtonGroup.Option>
+        <ButtonGroup.Option value={20}>Button 3</ButtonGroup.Option>
+      </ButtonGroup>
+      <Button border={'rounded'} circle>
+        Button
+      </Button>
     </>
   );
 }
--- src/components/ButtonGroup.stories.tsx
+++ src/components/ButtonGroup.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { useEffect, useMemo, useState } from 'react';
 
 import { ButtonGroup, ButtonGroupProps } from './ButtonGroup';
 
@@ -8,6 +9,15 @@ const meta: Meta<typeof ButtonGroup> = {
   parameters: {
     layout: 'centered',
   },
+  argTypes: {
+    value: {
+      options: [10, 20, 30],
+      control: { type: 'radio' },
+    },
+    border: {
+      control: { type: 'boolean' },
+    },
+  },
 };
 
 // TODO: figure out how to write stories for dot notation components
@@ -15,17 +25,63 @@ const meta: Meta<typeof ButtonGroup> = {
 
 export default meta;
 
-export const Plain = ({ value, onChange, variant }: ButtonGroupProps<number>) => {
+export const Oval = ({
+  value = 10,
+  onChange,
+  variant = 'oval',
+  border,
+}: ButtonGroupProps<number>) => {
+  const [stateValue, setStateValue] = useState(value);
+  useEffect(() => {
+    setStateValue(value);
+  }, [value]);
+
   return (
-    <ButtonGroup value={10} onChange={onChange} variant={'plain'}>
-      <ButtonGroup.Option plain value={10}>
-        First
+    <ButtonGroup
+      border={border}
+      value={stateValue}
+      onChange={(val) => setStateValue(val || 0)}
+      variant={variant}
+    >
+      <ButtonGroup.Option plain={variant === 'plain'} value={10}>
+        Button 1
       </ButtonGroup.Option>
-      <ButtonGroup.Option plain value={20}>
-        Second
+      <ButtonGroup.Option plain={variant === 'plain'} value={20}>
+        Button 2
       </ButtonGroup.Option>
-      <ButtonGroup.Option plain value={30}>
-        Third
+      <ButtonGroup.Option plain={variant === 'plain'} value={30}>
+        Button 3
+      </ButtonGroup.Option>
+    </ButtonGroup>
+  );
+};
+
+export const Plain = ({
+  value = 10,
+  onChange,
+  variant = 'plain',
+  border,
+}: ButtonGroupProps<number>) => {
+  const [stateValue, setStateValue] = useState(value);
+  useEffect(() => {
+    setStateValue(value);
+  }, [value]);
+
+  return (
+    <ButtonGroup
+      border={border}
+      value={stateValue}
+      onChange={(val) => setStateValue(val || 0)}
+      variant={variant}
+    >
+      <ButtonGroup.Option plain={variant === 'plain'} value={10}>
+        Button 1
+      </ButtonGroup.Option>
+      <ButtonGroup.Option plain={variant === 'plain'} value={20}>
+        Button 2
+      </ButtonGroup.Option>
+      <ButtonGroup.Option plain={variant === 'plain'} value={30}>
+        Button 3
       </ButtonGroup.Option>
     </ButtonGroup>
   );
