d455008d1f3af0ee42e54826e9e9e14a752bed2b
Anshul Goel <anshul.go87@gmail.com>
2023-02-22 16:58:55 +0530

    updates to popover and modal components

--- packages/@holaplexui-react/src/components/Popover.tsx
+++ packages/@holaplexui-react/src/components/Popover.tsx
@@ -1,15 +1,15 @@
 import { Popover, Transition } from '@headlessui/react';
 import clsx from 'clsx';
-import { Fragment } from 'react';
+import { Fragment, ReactNode } from 'react';
 
 export function PopoverBox({
   triggerButton,
   elements,
-  forceDirection,
+  children,
 }: {
   triggerButton: JSX.Element;
-  elements: JSX.Element[];
-  forceDirection?: 'left';
+  elements?: JSX.Element[];
+  children?: ReactNode;
 }) {
   return (
     <Popover className="relative">
@@ -25,19 +25,21 @@ export function PopoverBox({
             leaveFrom="transform scale-100 opacity-100"
             leaveTo="transform scale-95 opacity-0"
           >
-            <Popover.Panel
-              className={clsx(
-                'absolute z-20 w-screen max-w-sm translate-y-2 ',
-                forceDirection && '-translate-x-[calc(208px-40px)]'
-              )}
-            >
-              <ul className="popover overflow-hidden">
-                {elements.map((element) => (
-                  <li key={element.key} className="cursor-pointer">
-                    {element}
-                  </li>
-                ))}
-              </ul>
+            <Popover.Panel className={clsx('absolute z-20 w-auto', 'popover-panel')}>
+              <div className="overflow-hidden popover-content">
+                <>
+                  {elements && (
+                    <ul className="popover-elements">
+                      {elements.map((element) => (
+                        <li key={element.key} className="cursor-pointer">
+                          {element}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                  {!elements && children && { children }}
+                </>
+              </div>
             </Popover.Panel>
           </Transition>
         </>
