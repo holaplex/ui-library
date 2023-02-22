3a3d58aa9c147f677cefda718e9b5555f2ec48cf
Anshul Goel <anshul.go87@gmail.com>
2023-02-22 18:41:15 +0530

    add react-popper

--- packages/@holaplexui-react/src/components/Popover.tsx
+++ packages/@holaplexui-react/src/components/Popover.tsx
@@ -1,6 +1,7 @@
 import { Popover, Transition } from '@headlessui/react';
 import clsx from 'clsx';
-import { Fragment, ReactNode } from 'react';
+import { Fragment, ReactNode, useState } from 'react';
+import { usePopper } from 'react-popper';
 
 export function PopoverBox({
   triggerButton,
@@ -11,11 +12,16 @@ export function PopoverBox({
   elements?: JSX.Element[];
   children?: ReactNode;
 }) {
+  let [referenceElement, setReferenceElement] = useState<any>();
+  let [popperElement, setPopperElement] = useState<any>();
+  let { styles, attributes } = usePopper(referenceElement, popperElement);
   return (
     <Popover className="relative">
       {({ open }) => (
         <>
-          <Popover.Button as="div">{triggerButton}</Popover.Button>
+          <Popover.Button as="div" ref={setReferenceElement}>
+            {triggerButton}
+          </Popover.Button>
           <Transition
             as={Fragment}
             enter="transition duration-100 ease-out"
@@ -25,7 +31,12 @@ export function PopoverBox({
             leaveFrom="transform scale-100 opacity-100"
             leaveTo="transform scale-95 opacity-0"
           >
-            <Popover.Panel className={clsx('absolute z-20 w-auto', 'popover-panel')}>
+            <Popover.Panel
+              ref={setPopperElement}
+              style={styles.popper}
+              {...attributes.popper}
+              className={clsx('absolute z-20 w-auto', 'popover-panel')}
+            >
               <div className="overflow-hidden popover-content">
                 <>
                   {elements && (
